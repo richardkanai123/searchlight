c6890d8329db8b0dc7c8007f1b712fcd3aabfc4f
RichardKanai <richardpoetgamer@gmail.com>
2022-02-12 12:23:56 +0300

    Animation on entrypage added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-import { init } from "ityped";
-
+// ityped js
 const tagline = document.querySelector(".tagline");
-
-init(tagline, {
-  showCursor: false,
-  startDelay: 500,
-  loop: true,
-  strings: ["try", "One!!"],
+ityped.init(tagline, {
+  showCursor: true,
+  startDelay: 600,
+  loop: false,
+  backDelay: 500,
+  typeSpeed: 150,
+  strings: ["Here to make text speak for you!"],
 });
+
+// Gsap
+
+gsap.to(".banner_image", { duration: 2.5, y: -100, ease: "power1.out" });
